0fc234d825b70eceaff23775badc9061b94d96e7
habilarlian <143682187+habilarlian@users.noreply.github.com>
2023-09-27 20:46:59 +0800

    Add files via upload

--- adbil.js
+++ adbil.js
@@ -11,17 +11,45 @@ function tutup (){
     bars.style.display = "inline"
     close.style.display = "none"
 }
+
     const navbar = document.querySelector(".navbar")
+    const up = document.getElementById("arrow1")
     window.addEventListener("scroll",() =>{
         const positionwindow = window.scrollY >120;
         navbar.classList.toggle("scrolling-active",positionwindow)
     })
-
+    window.addEventListener("scroll",() =>{
+        const positionwindow = window.scrollY >580;
+        if (positionwindow){
+            up.style.display ="block"
+        }
+    })
+    window.addEventListener("scroll",() =>{
+        const positionwindow = window.scrollY <32;
+        if (positionwindow){
+            up.style.display ="none"
+            up.style.transition ="all .3s ease"
+        }
+    })
+    window.addEventListener("scroll",() =>{
+        const positionwindow = window.scrollY >580;
+        if (positionwindow){
+            up.style.display ="block"
+            up.style.transition ="all .3s ease"
+        }
+    })
+    window.addEventListener('click',(e) => {
+        if (e.target != bars && e.target != menu){
+            tutup()
+        }
+    })
+    window.addEventListener('scroll',(e) => {
+        if (e.target != bars && e.target != menu){
+            tutup()
+        }
+    })
     // swiper effect start
 const swiper = new Swiper('.swiper', {
-    autoplay :{
-        delay :3000,
-    },
     // Optional parameters
     loop: true,
 
